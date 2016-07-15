ac65b825f3bccbfea00f43a38e01f40250cc2e2b
Pierre Rudloff <contact@rudloff.pro>
2016-07-15 03:05:43 +0200

    WIP

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+class SignalSpamPlugin extends \RainLoop\Plugins\AbstractPlugin
+{
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        $this->addHook('filter.action-params', 'sendToSignalSpam');
+    }
+
+    /**
+     * @return array
+     */
+    public function configMapping()
+    {
+        $oUser = \RainLoop\Plugins\Property::NewInstance('user')->SetLabel('Signal Spam username');
+        $oPass = \RainLoop\Plugins\Property::NewInstance('pass')->SetLabel('Signal Spam password');
+
+        return array($oUser, $oPass);
+    }
+
+    public function sendToSignalSpam($sMethodName, $aCurrentActionParams)
+    {
+        if ($sMethodName == 'DoMessageMove') {
+            $actions = $this->Manager()->Actions();
+            $oAccount = $actions->getAccountFromToken();
+            $spamFolder = $actions->SettingsProvider(true)->Load($oAccount)->GetConf('SpamFolder', '');
+
+            if ($aCurrentActionParams['ToFolder'] == $spamFolder) {
+                $oAccount->IncConnectAndLoginHelper($actions->Plugins(), $actions->MailClient(), $actions->Config());
+                $mailClient = $actions->MailClient();
+                $aUids = explode(',', $aCurrentActionParams['Uids']);
+				foreach ($aUids as $iUid) {
+	                $mailClient->MessageMimeStream(function ($rResource) {
+	                    if (\is_resource($rResource)) {
+	                        $client = new \GuzzleHttp\Client();
+	                        $mailContent = stream_get_contents($rResource);
+	                        $res = $client->request(
+	                            'POST',
+	                            'https://www.signal-spam.fr/api/signaler',
+	                            array(
+	                                'auth' => array(
+	                                    $this->Config()->Get('plugin', 'user', ''),
+	                                    $this->Config()->Get('plugin', 'pass', '')
+	                                ),
+	                                'multipart'=>array(
+	                                    array(
+	                                        'name'=>'message',
+	                                        'contents'=>base64_encode($mailContent)
+	                                    )
+	                                )
+	                            )
+	                        );
+	                    }
+	                }, $aCurrentActionParams['FromFolder'], intval($iUid));
+				}
+            }
+        }
+    }
+}
